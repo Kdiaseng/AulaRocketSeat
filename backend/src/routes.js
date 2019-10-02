41cb7e435963544a0e6f9380375c96bba1a55ea9
kaleb.silva <kaleb.silva@itbam.org.br>
2019-10-02 08:22:26 -0400

    controller de spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,7 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.store);
+routes.post('/spots', SessionController.store);
+
 
 module.exports = routes;
