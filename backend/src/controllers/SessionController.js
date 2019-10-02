f959e2cd97dde0ada658e7ece2871bbaecd44efd
kaleb.silva <kaleb.silva@itbam.org.br>
2019-10-01 22:00:58 -0400

    cadastro de email

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,7 +1,23 @@
 // index, show, store, update, destroy
 
+const User = require('../models/User')
+
 module.exports = {
-   store(req , res){
-       return res.json({message : 'Hello World'})
+    async store(req , res){
+       const { email } = req.body;
+
+       //forma simplificada para setar email ja que é o mesmo nome
+       let user = await User.findOne({email});
+
+       if (!user){
+            user = await User.create({ email });
+       }
+
+       //await espera até finalizar a requisicao
+       // const user = await User.create({ email });
+
+
+       return res.json(user);
+     
    }
 }
